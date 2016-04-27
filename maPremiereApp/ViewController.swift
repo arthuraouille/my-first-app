bf55bb16785bb52f5fa081717abaddff529b8867
Arthur Aouillé <aaouille@gmail.com>
2016-04-27 22:18:48 +0200

    Initial Commit

---
+++ maPremiereApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  maPremiereApp
+//
+//  Created by Arthur Aouillé on 27/04/2016.
+//  Copyright © 2016 Arthur Aouillé. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
